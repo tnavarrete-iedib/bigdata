6b4b9d9a9b7b03e773b2e7ce4b2667ec7cfe45a2
tnavarrete-iedib <120335711+tnavarrete-iedib@users.noreply.github.com>
2023-04-25 12:59:07 +0200

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,25 @@
+# Especificamos las versiones que se utilizarán de Spark, Hadoop y JupyterLab
+SPARK_VERSION="3.4.0"
+HADOOP_VERSION="3.3.5"
+JUPYTERLAB_VERSION="3.6.3"
+
+# Creación de la imagen de Docker spark-master
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg hadoop_version="${HADOOP_VERSION}" \
+  -f spark-master.Dockerfile \
+  -t spark-master .
+
+# Creación de la imagen de Docker spark-worker
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg hadoop_version="${HADOOP_VERSION}" \
+  -f spark-worker.Dockerfile \
+  -t spark-worker .
+
+# Creación de la imagen de Docker jupyterlab
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
+  -f jupyterlab.Dockerfile \
+  -t jupyterlab .
